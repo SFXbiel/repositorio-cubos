2a21e0af7f99372124171fb8218d7ee25e771c37
Gabriel Sousa Rangel <gabrielsousarangel5.az2@gmail.com>
2023-11-13 22:24:23 -0300

    Exercitando1

---
+++ aprendendoAProgramar.js
@@ -0,0 +1,35 @@
+// VARIAVEIS: onde damos um nome e colocamos um tipo de dado; (Utilizamos let e Const)
+let variavel = "Papel"
+console.log(variavel);
+
+//não podemos redefinir com a varaiavel const, por isso não irá funcionar.
+// const conl = "hello"
+
+// conl = "Wolrd"
+// console.log(conl)
+
+// -----------------------------------------------------
+const numero = 0;
+ console.log(`Eu tenho um ${variavel} com ${numero} anos de idade`)
+
+ //-------------------------------------------------------
+  const cidadeNatal = 'Campos dos Goytacazes'
+  console.log(`A minha cidade Natal é ${cidadeNatal}`)
+  //------------------------------------------------------
+
+  let populacao = 500.000;
+  console.log(populacao)
+  populacao = 510_000;
+  console.log(populacao)
+  //------------------------------------------------------
+  //Treinando escopo
+  {
+    const age = 5;
+    const idade = 8;
+     {
+        console.log(age)
+        console.log(idade)
+        let nome = "jyg"
+     }
+    console.log(nome);
+  }

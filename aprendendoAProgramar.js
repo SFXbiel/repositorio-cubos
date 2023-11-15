182ccb573f525ebf59bff3f14c86f056808eef90
Gabriel Sousa Rangel <gabrielsousarangel5.az2@gmail.com>
2023-11-14 22:17:01 -0300

    exercitando na aula

--- aprendendoAProgramar.js
+++ aprendendoAProgramar.js
@@ -1,6 +1,6 @@
 // VARIAVEIS: onde damos um nome e colocamos um tipo de dado; (Utilizamos let e Const)
-let variavel = "Papel"
-console.log(variavel);
+// let variavel = "Papel"
+// console.log(variavel);
 
 //não podemos redefinir com a varaiavel const, por isso não irá funcionar.
 // const conl = "hello"
@@ -9,27 +9,43 @@ console.log(variavel);
 // console.log(conl)
 
 // -----------------------------------------------------
-const numero = 0;
- console.log(`Eu tenho um ${variavel} com ${numero} anos de idade`)
+// const numero = 0;
+//  console.log(`Eu tenho um ${variavel} com ${numero} anos de idade`)
 
- //-------------------------------------------------------
-  const cidadeNatal = 'Campos dos Goytacazes'
-  console.log(`A minha cidade Natal é ${cidadeNatal}`)
-  //------------------------------------------------------
+//  //-------------------------------------------------------
+//   const cidadeNatal = 'Campos dos Goytacazes'
+//   console.log(`A minha cidade Natal é ${cidadeNatal}`)
+//   //------------------------------------------------------
 
-  let populacao = 500.000;
-  console.log(populacao)
-  populacao = 510_000;
-  console.log(populacao)
-  //------------------------------------------------------
-  //Treinando escopo
-  {
-    const age = 5;
-    const idade = 8;
-     {
-        console.log(age)
-        console.log(idade)
-        let nome = "jyg"
-     }
-    console.log(nome);
-  }
+//   let populacao = 500.000;
+//   console.log(populacao)
+//   populacao = 510_000;
+//   console.log(populacao)
+//   //------------------------------------------------------
+//   //Treinando escopo
+//   {
+//     const age = 5;
+//     const idade = 8;
+//      {
+//         console.log(age)
+//         console.log(idade)
+//         let nome = "jyg"
+//      }
+//     console.log(nome);
+//   }
+//--------------------------------------------------------
+//  EXERCICIO 3 (14.11.2023)
+let x = 10, y = 2;
+let resultado = x + y;
+console.log(`A soma de ${x} e ${y} é ${resultado}`);
+
+resultado = x - y;
+console.log(`A subtração de ${x} e ${y} é ${resultado}`);
+
+resultado = x * y;
+console.log(`A Multiplicação de ${x} e ${y}, é ${resultado}`);
+
+resultado = x / y;
+console.log(`A Divizão de ${x} e ${y}, é ${resultado}`);
+console.log('Booooooooaaaaaaa')
+//
